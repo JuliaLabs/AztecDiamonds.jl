df438edc50e720651be4cbcd93459f05d6a7d67e
Simeon David Schaub <schaub@mit.edu>
2023-01-11 17:15:57 +0100

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,33 @@
 using AztecDiamonds
+using AztecDiamonds: diamond, Tiling, inds, NONE, UP, RIGHT
 using Test
 
+function verify_tiling(t::Tiling)
+    (; N, x) = t
+    for (i, j) in Iterators.product(inds(N)...)
+        if checkbounds(Bool, t, i, j)
+            if t[i, j] == NONE && get(t, (i-1, j), NONE) != UP && get(t, (i, j-1), NONE) != RIGHT
+                error("Square ($i, $j) is not covered by any tile!")
+            end
+        else
+            if x[i, j] != NONE
+                error("Square ($i, $j) should be empty, is $(x[i, j])")
+            end
+            if get(x, CartesianIndex(i-1, j), NONE) == UP
+                error("Square ($i, $j) should be empty, is covered from below by ($(i-1), $j)")
+            end
+            if get(x, CartesianIndex(i, j-1), NONE) == RIGHT
+                error("Square ($i, $j) should be empty, is covered from the left by ($i, $(j-1))")
+            end
+        end
+    end
+    return true
+end
+
 @testset "AztecDiamonds.jl" begin
-    # Write your tests here.
+    D = diamond(100)
+    @test verify_tiling(D)
+
+    dr_path = AztecDiamonds.dr_path(D)
+    @test dr_path[end] == -0.5
 end
