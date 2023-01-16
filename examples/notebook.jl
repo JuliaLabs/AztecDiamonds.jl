340f661c65940a58e09aa2e8bb0268c7790c9a42
Simeon David Schaub <schaub@mit.edu>
2023-01-17 00:04:45 +0100

    wip

--- examples/notebook.jl
+++ examples/notebook.jl
@@ -7,6 +7,9 @@ using InteractiveUtils
 # ╔═╡ a609b8a8-04ac-4533-9a33-61ea33805846
 using AztecDiamonds, Plots
 
+# ╔═╡ 73be3cb4-3106-441c-8244-9f0d831dbcca
+using Adapt
+
 # ╔═╡ ecde5a72-691b-4a9a-b0a8-2b740e42a710
 D = diamond(200)
 
@@ -25,10 +28,12 @@ to_img(adapt(Array, cuda_diamond(2000)))
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
 [deps]
+Adapt = "79e6a3ab-5dfb-504d-930d-738a2a938a0e"
 AztecDiamonds = "8762d9c5-fcab-4007-8fd1-c6de73397726"
 Plots = "91a5bcdd-55d7-5caf-9e0b-520d859cae80"
 
 [compat]
+Adapt = "~3.4.0"
 AztecDiamonds = "~0.1.0"
 Plots = "~1.38.2"
 """
@@ -39,7 +44,7 @@ PLUTO_MANIFEST_TOML_CONTENTS = """
 
 julia_version = "1.8.5"
 manifest_format = "2.0"
-project_hash = "78ee2fbdf79e78657d39945c8a5b5e0a786baa8c"
+project_hash = "3c71902f922761b4af1837f59da88af29a674eea"
 
 [[deps.AbstractFFTs]]
 deps = ["ChainRulesCore", "LinearAlgebra"]
@@ -1386,6 +1391,7 @@ version = "1.4.1+0"
 # ╠═ecde5a72-691b-4a9a-b0a8-2b740e42a710
 # ╠═1cf94d6d-a0bc-474b-b479-5b4f4c916ea5
 # ╠═8bb0983b-103e-4cf8-9a9f-95feb90df054
+# ╠═73be3cb4-3106-441c-8244-9f0d831dbcca
 # ╠═698b9232-4117-421f-b561-6c93d606f6b9
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
