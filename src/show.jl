a19325cd6c839538b0c3aca4a9433033697db2df
Simeon Schaub <simeondavidschaub99@gmail.com>
2023-02-21 08:15:26 -0500

    add REPL pretty printing

--- src/show.jl
+++ src/show.jl
@@ -17,6 +17,49 @@ function to_img(t::Tiling)
     img
 end
 
+function Base.show(io::IO, t::Tiling)
+    summary(io, t)
+    (; N) = t
+    displaysize(io)[2] ≥ 2N || return print(io, "\n  Output too large to fit terminal. Use \
+        `using ImageView; imshow(AztecDiamonds.to_img(D))` to display as an image instead.")
+    t = adapt(Array, t)
+    foreach(Iterators.product(inds(N)...)) do (j, i)
+        j == 1-N && println(io)
+        isdotted = isodd(i+j-N)
+        if get(t, (i, j), NONE) == UP
+            color = isdotted ? :red : :green
+            if get(t, (i-1, j), NONE) == UP
+                print(io, 'a')
+            elseif get(t, (i, j-1), NONE) == RIGHT
+                print(io, 'b')
+            else
+                printstyled(io, '╻'; color)
+            end
+        elseif get(t, (i-1, j), NONE) == UP
+            color = !isdotted ? :red : :green
+            if get(t, (i, j-1), NONE) == RIGHT
+                print(io, 'c')
+            else
+                printstyled(io, '╹'; color)
+            end
+        elseif get(t, (i, j), NONE) == RIGHT
+            color = isdotted ? :yellow : :blue
+            if get(t, (i-1, j), NONE) == UP
+                print(io, 'd')
+            elseif get(t, (i, j-1), NONE) == RIGHT
+                print(io, 'e')
+            else
+                printstyled(io, '╺'; color)
+            end
+        elseif get(t, (i, j-1), NONE) == RIGHT
+            color = !isdotted ? :yellow : :blue
+            printstyled(io, '╸'; color)
+        else
+            print(io, ' ')
+        end
+    end
+end
+
 Base.showable(::MIME"image/png", (; N)::Tiling) = N > 0
 
 function Base.show(io::IO, ::MIME"image/png", t::Tiling; kw...)
