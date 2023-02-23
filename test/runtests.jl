565d9ca34d43914ded4e6f3c9d4bb67ab231fdf5
Simeon David Schaub <schaub@mit.edu>
2023-02-23 17:31:17 +0100

    verbose test output

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,13 @@
+# TODO: remove once TestItemRunner v0.2.2 is released
+import Pkg; Pkg.add(Pkg.PackageSpec(; name="TestItemRunner", rev="main"))
 using TestItemRunner, CUDA
 
 iscuda((; tags)) = :cuda in tags
 
 if !(haskey(ENV, "BUILDKITE") && CUDA.functional()) # skip non-gpu tests on Buildkite CI
-    @run_package_tests filter=!iscuda #verbose=true
+    @run_package_tests filter=!iscuda verbose=true
 end
 
 if CUDA.functional()
-    @run_package_tests filter=iscuda #verbose=true
+    @run_package_tests filter=iscuda verbose=true
 end
