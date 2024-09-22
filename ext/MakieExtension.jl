eb2747c060acf409d0a28ab090ed6a9b6ef41e9e
Simeon David Schaub <simeon@schaub.rocks>
2024-09-22 15:06:05 +0200

    use package extensions for Makie dep (#22)

--- ext/MakieExtension.jl
+++ ext/MakieExtension.jl
@@ -1,5 +1,12 @@
-using MakieCore
+module MakieExtension
+
+using Makie
 using GeometryBasics: Vec2f, Point2f, Rect2f
+using Colors
+using Adapt: adapt
+
+using AztecDiamonds: Tiling, faces, UP, RIGHT
+import AztecDiamonds: tilingplot, tilingplot!
 
 function prepare_plot(t::Tiling; pad = 0.1f0)
     tiles = Rect2f[]
@@ -27,20 +34,22 @@ function prepare_plot(t::Tiling; pad = 0.1f0)
     return tiles, colors, arrow_pts, arrows
 end
 
-MakieCore.@recipe(TilingPlot, t) do scene
-    MakieCore.Attributes(
+@recipe(TilingPlot, t) do scene
+    Attributes(
         show_arrows = false,
         domino_padding = 0.1f0,
         domino_stroke = 0,
     )
 end
 
-MakieCore.plottype(::Tiling) = TilingPlot
+Makie.plottype(::Tiling) = TilingPlot
 
-function MakieCore.plot!(x::TilingPlot{<:Tuple{Tiling}})
+function Makie.plot!(x::TilingPlot{<:Tuple{Tiling}})
     t = adapt(Array, x[:t][])
     tiles, colors, arrow_pts, arrows = prepare_plot(t; pad = x.domino_padding[])
     poly!(x, tiles; color = colors, strokewidth = x.domino_stroke)
     x.show_arrows[] && arrows!(x, arrow_pts, arrows)
     return x
+end
+
 end
