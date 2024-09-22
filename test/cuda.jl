d569122e69259b7857cf5947390ab90f88fba330
Simeon David Schaub <simeon@schaub.rocks>
2024-09-22 13:33:18 +0200

    add CUDA tests

--- test/cuda.jl
+++ test/cuda.jl
@@ -5,4 +5,11 @@
     D = ka_diamond(200, CuArray)
     D_cpu = adapt(Array, D)
     @test verify_tiling(D_cpu)
+
+    @testset "$rot" for rot in (rotr90, rotl90, rot180)
+        D′ = rot(D)
+        D_cpu′ = adapt(Array, D′)
+        @test verify_tiling(D_cpu′)
+        @test D_cpu′ == rot(D_cpu)
+    end
 end
