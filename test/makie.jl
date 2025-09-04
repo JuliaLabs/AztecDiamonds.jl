66937456bafc505fe83a8453017b189fb5000d3c
Simeon David Schaub <simeon@schaub.rocks>
2025-09-04 09:30:34 +0200

    improve test coverage

--- test/makie.jl
+++ test/makie.jl
@@ -5,7 +5,7 @@
 
     f = Figure()
     ax = Axis(f[1, 1]; aspect = 1)
-    plot!(ax, D; domino_padding = 0.05f0, domino_stroke = 1, show_arrows = true)
+    plot!(ax, D; domino_padding = 0.05f0, domino_stroke = 1, show_arrows = true, colormap = [:black, :gray60, :gray60, :black])
 
     path = tempname() * ".png"
     save(path, f)
