3277194886e66c0f7e3688740491bebf1e2ff730
Simeon David Schaub <simeon@schaub.rocks>
2025-09-23 19:20:38 +0200

    add test

--- test/makie.jl
+++ test/makie.jl
@@ -4,8 +4,9 @@
     D = diamond(100)
 
     f = Figure()
-    ax = Axis(f[1, 1]; aspect = 1)
+    ax = Axis(f[1, 1]; autolimitaspect = 1)
     plot!(ax, D; domino_padding = 0.05f0, domino_stroke = 1, show_arrows = true)
+    image(f[1, 2], D; axis = (; autolimitaspect = 1))
 
     path = tempname() * ".png"
     save(path, f)
